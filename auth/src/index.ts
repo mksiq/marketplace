ee52b8eafb5a24906325892cac38c7ceb1e3950e
mksiq <maickelsiqueira@gmail.com>
2021-05-02 19:19:57 -0400

    added config for deployment on google cloud

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,7 +4,11 @@ const app = express();
 app.use(json());
 
 app.listen(3000, () => {
-  console.log('Listening on 3000');
+  console.log('Listening on 3000.');
+});
+
+app.get('/api/users/currentuser', (req, res) => {
+  res.send('User test');
 });
 
 app.get('/', (req, res) => {
