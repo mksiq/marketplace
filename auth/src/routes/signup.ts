c0c930dfe828f660fcf04cd893a3bdfe8b3853da
mksiq <maickelsiqueira@gmail.com>
2021-05-02 19:54:18 -0400

    added express-validator to auth signup route body

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,9 +1,27 @@
 import express from 'express';
+import { body } from 'express-validator';
 
 const router = express.Router();
 
-router.post('/api/users/signup', (req, res) => {
-  res.send('User signed up.');
-});
+router.post(
+  '/api/users/signup',
+  [
+    body('email')
+      .isEmail()
+      .normalizeEmail()
+      .withMessage('Please provide a valid email'),
+    body('password')
+      .trim()
+      .isLength({ min: 5, max: 40 })
+      .withMessage(
+        'Please provide a password with at least 5 chars and at most 40 chars'
+      ),
+  ],
+  (req: express.Request, res: express.Response) => {
+    const { email, password } = req.body;
+
+    res.send('User signed up.');
+  }
+);
 
 export { router as signupRouter };
