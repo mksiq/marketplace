8c9b7be225aa8d93c596e818945f8fc984157300
mksiq <maickelsiqueira@gmail.com>
2021-05-14 10:00:02 -0400

    auth: user signup with bad request handling

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -6,6 +6,17 @@ interface UserAttrs {
   password: string;
 }
 
+// An interface that describes to typescript the new functions to schema
+interface UserModel extends mongoose.Model<UserDoc> {
+  build(attrs: UserAttrs): UserDoc;
+}
+
+// an interface to describe what properties exist in a User Document
+interface UserDoc extends mongoose.Document {
+  email: string;
+  password: string;
+}
+
 const userSchema = new mongoose.Schema({
   email: {
     type: String,
@@ -18,10 +29,11 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-const User = mongoose.model('User', userSchema);
-
-const buildUser = (attrs: UserAttrs) => {
+// to add static method to schema
+userSchema.statics.build = (attrs: UserAttrs) => {
   return new User(attrs);
 };
 
-export { User, buildUser };
+const User = mongoose.model<UserDoc, UserModel>('User', userSchema);
+
+export { User };
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,7 +1,8 @@
 import express from 'express';
 import { body, validationResult } from 'express-validator';
-import { DatabaseConnectionError } from '../errors/database-connection-error';
+import { BadRequestError } from '../errors/bad-request-error';
 import { RequestValidationError } from '../errors/request-validation-error';
+import { User } from '../models/user';
 
 const router = express.Router();
 
@@ -19,7 +20,7 @@ router.post(
         'Please provide a password with at least 5 chars and at most 40 chars'
       ),
   ],
-  (req: express.Request, res: express.Response) => {
+  async (req: express.Request, res: express.Response) => {
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
@@ -28,7 +29,16 @@ router.post(
 
     const { email, password } = req.body;
 
-    res.send({ email, password });
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      throw new BadRequestError('User already exists');
+    }
+
+    const user = User.build({ email, password });
+    await user.save();
+
+    res.status(201).send(user);
   }
 );
 
