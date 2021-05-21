f4455133b43190d14615cc0b22aeecdcf52bd150
mksiq <maickelsiqueira@gmail.com>
2021-05-21 10:30:44 -0400

    auth: added jwt

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,20 +1,35 @@
 import express, { json } from 'express';
+import mongoose from 'mongoose';
+import cookieSession from 'cookie-session';
 import 'express-async-errors';
+
 import { NotFoundError } from './errors/not-found-error';
 import { currentUserRouter } from './routes/current-user';
 import { errorHandler } from './routes/middlewares/error-handler';
 import { signinRouter } from './routes/signin';
 import { signoutRouter } from './routes/signout';
 import { signupRouter } from './routes/signup';
-import mongoose from 'mongoose';
 
 const app = express();
+
+// extra security for cookieSession
+app.set('trust proxy', true);
 app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true,
+  })
+);
 
 app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
+
+app.all('*', async (req, res) => {
+  throw new NotFoundError();
+});
 
 app.get('/', (req, res) => {
   res.json({
@@ -29,6 +44,10 @@ app.get('*', () => {
 app.use(errorHandler);
 
 const init = async () => {
+  if (!process.env.JWT_KEY) {
+    throw new Error('JWT_KEY must be defined');
+  }
+
   try {
     await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
       useCreateIndex: true,
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 import { body, validationResult } from 'express-validator';
+import jwt from 'jsonwebtoken';
+
 import { BadRequestError } from '../errors/bad-request-error';
 import { RequestValidationError } from '../errors/request-validation-error';
 import { User } from '../models/user';
@@ -9,16 +11,11 @@ const router = express.Router();
 router.post(
   '/api/users/signup',
   [
-    body('email')
-      .isEmail()
-      .normalizeEmail()
-      .withMessage('Please provide a valid email'),
+    body('email').isEmail().normalizeEmail().withMessage('Please provide a valid email'),
     body('password')
       .trim()
       .isLength({ min: 5, max: 40 })
-      .withMessage(
-        'Please provide a password with at least 5 chars and at most 40 chars'
-      ),
+      .withMessage('Please provide a password with at least 5 chars and at most 40 chars'),
   ],
   async (req: express.Request, res: express.Response) => {
     const errors = validationResult(req);
@@ -37,6 +34,22 @@ router.post(
 
     const user = User.build({ email, password });
     await user.save();
+
+    // Generate JWT
+    const userJwt = jwt.sign(
+      {
+        id: user.id,
+        email: user.email,
+      },
+      process.env.JWT_KEY! // ! mark forces typescript to not care about the possible undefined, it was defined on init function in index.js
+    );
+
+    console.log(userJwt);
+
+    // Store on session JWT
+    req.session = {
+      jwt: userJwt,
+    };
 
     res.status(201).send(user);
   }
