c528b90fc6a95e23c8a16a7b603e11aa820f9aef
mksiq <maickelsiqueira@gmail.com>
2021-05-23 09:48:00 -0400

    auth: refactored index.ts to break features

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,43 +1,5 @@
-import express, { json } from 'express';
 import mongoose from 'mongoose';
-import cookieSession from 'cookie-session';
-import 'express-async-errors';
-
-import { NotFoundError } from './errors/not-found-error';
-import { currentUserRouter } from './routes/current-user';
-import { errorHandler } from './routes/middlewares/error-handler';
-import { signinRouter } from './routes/signin';
-import { signoutRouter } from './routes/signout';
-import { signupRouter } from './routes/signup';
-
-const app = express();
-
-// extra security for cookieSession
-app.set('trust proxy', true);
-app.use(json());
-app.use(
-  cookieSession({
-    signed: false,
-    secure: true,
-  })
-);
-
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
-app.use(signupRouter);
-
-app.get('/', (req, res) => {
-  res.json({
-    message: 'Auth Service listening for requests',
-  });
-});
-
-app.all('*', async (req, res) => {
-  throw new NotFoundError();
-});
-
-app.use(errorHandler);
+import { app } from './app';
 
 const init = async () => {
   if (!process.env.JWT_KEY) {
