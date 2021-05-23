58c3e493415b0d6f776479c9d4d4c883b1eb772f
mksiq <maickelsiqueira@gmail.com>
2021-05-23 16:13:19 -0400

    auth: refactore signin cookies to be reusable and new currentuser test

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -2,17 +2,15 @@ import request from 'supertest';
 import { app } from '../../app';
 
 it('contains response with current user', async () => {
-  const res = await request(app)
-    .post('/api/users/signup')
-    .send({
-      email: 'maicke@aaaa.aa',
-      password: '12345',
-    })
-    .expect(201);
-
-  const cookie = res.get('Set-Cookie');
+  const cookie = await global.signin();
 
   const responseCurrentUser = await request(app).get('/api/users/currentUser').set('Cookie', cookie).send().expect(200);
 
   expect(responseCurrentUser.body.currentUser.email).toEqual('maicke@aaaa.aa');
+});
+
+it('returns null if not authenticated', async () => {
+  const responseCurrentUser = await request(app).get('/api/users/currentUser').send().expect(200);
+
+  expect(responseCurrentUser.body.currentUser).toBeNull();
 });
--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,12 +1,21 @@
 import { MongoMemoryServer } from 'mongodb-memory-server';
+import request from 'supertest';
 import mongoose from 'mongoose';
 import { app } from '../app';
+
+declare global {
+  namespace NodeJS {
+    interface Global {
+      signin(): Promise<Array<string>>;
+    }
+  }
+}
 
 let mongodb: any;
 
 beforeAll(async () => {
   mongodb = new MongoMemoryServer();
-  process.env.JWT_KEY = 'change-this-jwt-key-in-use';
+  process.env.JWT_KEY = 'tHiSiSaSeCrEt';
   const mongoUri = await mongodb.getUri();
 
   await mongoose.connect(mongoUri, {
@@ -27,3 +36,18 @@ afterAll(async () => {
   await mongodb.stop();
   await mongoose.connection.close();
 });
+
+global.signin = async () => {
+  const email = 'maicke@aaaa.aa';
+  const password = '12345';
+
+  const res = await request(app)
+    .post('/api/users/signup')
+    .send({
+      email,
+      password,
+    })
+    .expect(201);
+
+  return res.get('Set-Cookie');
+};
